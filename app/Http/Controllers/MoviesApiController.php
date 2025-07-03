07e62e7283e6949f6c05058a2e5148c306c7acb4
Lesedi <lesedimautsoa2@gmail.com>
2025-07-03 10:27:21 +0200

    implementcaching

--- app/Http/Controllers/MoviesApiController.php
+++ app/Http/Controllers/MoviesApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Services\MoviesApiService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Cache;
 
 class MoviesApiController extends Controller
 {
@@ -15,43 +16,46 @@ class MoviesApiController extends Controller
         $this->movies = $movies;
     }
 
-    //return a list of movies
+    // Return a filtered list of cinemas (cache for 7 days)
     public function cinemas()
     {
-        $cinemas = $this->movies->getCinemas();
+        $cinemas = Cache::remember('cinemas_list', now()->addDays(7), function () {
+            return $this->movies->getCinemas();
+        });
 
-        // Only keep cinemas with these IDs
         $allowedIds = [44, 25, 9, 39, 40];
 
-        // If data is wrapped in a "data" key
         $filtered = collect($cinemas['data'] ?? [])
             ->whereIn('cinema_id', $allowedIds)
-            ->values(); // reset array keys
+            ->values();
 
         return response()->json($filtered);
     }
 
+    // Return movies for a specific cinema (cache for 6 days)
     public function movies($cinemaId)
-{
-    $movies = $this->movies->getMoviesByCinema($cinemaId);
-    
-    if (isset($movies['data']) && is_array($movies['data'])) {
-        foreach ($movies['data'] as $key => $movie) {
-            if (!empty($movie['movie_poster']) && !empty($movie['movie_id'])) {
-                $posterUrl = $this->movies->getPosterUrl(
-                    $movie['movie_id'], 
-                    $movie['movie_poster'], 
-                    216
-                );
-                $movies['data'][$key]['poster_url'] = $posterUrl;
-            } else {
-                $movies['data'][$key]['poster_url'] = asset('images/default-poster.jpg');
+    {
+        $cacheKey = 'movies_cinema_' . $cinemaId;
+
+        $movies = Cache::remember($cacheKey, now()->addDays(6), function () use ($cinemaId) {
+            return $this->movies->getMoviesByCinema($cinemaId);
+        });
+
+        if (isset($movies['data']) && is_array($movies['data'])) {
+            foreach ($movies['data'] as $key => $movie) {
+                if (!empty($movie['movie_poster']) && !empty($movie['movie_id'])) {
+                    $posterUrl = $this->movies->getPosterUrl(
+                        $movie['movie_id'],
+                        $movie['movie_poster'],
+                        216
+                    );
+                    $movies['data'][$key]['poster_url'] = $posterUrl;
+                } else {
+                    $movies['data'][$key]['poster_url'] = asset('images/default-poster.jpg');
+                }
             }
         }
-    }
-    
-    return response()->json($movies);
-}
-    
 
+        return response()->json($movies);
+    }
 }
