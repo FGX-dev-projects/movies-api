c99231ba40fa0b79e56811ba4ba1e427317af3c9
Lesedi <lesedimautsoa2@gmail.com>
2025-07-28 11:57:18 +0200

    remove-logs

--- app/Http/Controllers/MoviesApiController.php
+++ app/Http/Controllers/MoviesApiController.php
@@ -22,15 +22,15 @@ class MoviesApiController extends Controller
     {
         $cacheKey = 'cinemas_list';
         $cached = Cache::has($cacheKey);
-        Log::info('Checking cinemas cache', ['cache_key' => $cacheKey, 'cached' => $cached]);
+        // Log::info('Checking cinemas cache', ['cache_key' => $cacheKey, 'cached' => $cached]);
 
         $cinemas = Cache::remember($cacheKey, now()->addDays(30), function () {
-            Log::info('Fetching fresh data for getCinemas');
+            // Log::info('Fetching fresh data for getCinemas');
             $response = $this->movies->getCinemas();
             return is_array($response) && isset($response['data']) ? $response : ['data' => []];
         });
 
-        Log::info('cinemas data after cache', ['cinemas' => $cinemas]);
+        // Log::info('cinemas data after cache', ['cinemas' => $cinemas]);
 
         //$allowedIds = [44, 25, 9, 39, 40];
         //$allowedIds = [9, 11, 12, 13];
@@ -49,15 +49,15 @@ class MoviesApiController extends Controller
     {
         $cacheKey = 'movies_cinema_' . $cinemaId;
         $cached = Cache::has($cacheKey);
-        Log::info('Checking movies cache', ['cache_key' => $cacheKey, 'cinema_id' => $cinemaId, 'cached' => $cached]);
+        // Log::info('Checking movies cache', ['cache_key' => $cacheKey, 'cinema_id' => $cinemaId, 'cached' => $cached]);
 
         $movies = Cache::remember($cacheKey, now()->next('Monday'), function () use ($cinemaId) {
-            Log::info('Fetching fresh data for getMoviesByCinema', ['cinema_id' => $cinemaId]);
+            // Log::info('Fetching fresh data for getMoviesByCinema', ['cinema_id' => $cinemaId]);
             $response = $this->movies->getMoviesByCinema($cinemaId);
             return is_array($response) && isset($response['data']) ? $response : ['data' => []];
         });
 
-        Log::info('movies data after cache', ['cinema_id' => $cinemaId, 'movies' => $movies]);
+        // Log::info('movies data after cache', ['cinema_id' => $cinemaId, 'movies' => $movies]);
 
         if (isset($movies['data']) && is_array($movies['data'])) {
             foreach ($movies['data'] as $key => $movie) {
